0d7cc8ec1e293febbbc61cca80578648dfea55fd
bergoid <bergoid@users.noreply.github.com>
2024-02-20 20:41:05 +0100

    Default colors: meco theme

--- src/suckless/common/defaults.h
+++ src/suckless/common/defaults.h
@@ -3,37 +3,37 @@
 // E.g.: export LOCAL_DWM_FONT="Inconsolata:size=12:antialias=true:autohint=true"
 #define DEFAULT_FONT "monospace:size=13"
 
+// Terminal colors 0-15
+#define DEFAULT_COLOR_00 "#101010"
+#define DEFAULT_COLOR_01 "#c04040"
+#define DEFAULT_COLOR_02 "#48c848"
+#define DEFAULT_COLOR_03 "#c8c848"
+#define DEFAULT_COLOR_04 "#4888c8"
+#define DEFAULT_COLOR_05 "#c040c0"
+#define DEFAULT_COLOR_06 "#40c0c0"
+#define DEFAULT_COLOR_07 "#c0c0c0"
+#define DEFAULT_COLOR_08 "#404040"
+#define DEFAULT_COLOR_09 "#e05050"
+#define DEFAULT_COLOR_10 "#50e050"
+#define DEFAULT_COLOR_11 "#e0e050"
+#define DEFAULT_COLOR_12 "#68b8f8"
+#define DEFAULT_COLOR_13 "#e050e0"
+#define DEFAULT_COLOR_14 "#50e0e0"
+#define DEFAULT_COLOR_15 "#e0e0e0"
+
 // The foreground text color
 // Override with the environment variable LOCAL_DWM_FG_COLOR
-#define DEFAULT_FG_COLOR "#989898"
+#define DEFAULT_FG_COLOR DEFAULT_COLOR_07
 
 // The background color
 // Override with the environment variable LOCAL_DWM_BG_COLOR
-#define DEFAULT_BG_COLOR "#101010"
+#define DEFAULT_BG_COLOR DEFAULT_COLOR_00
 
 // The color of the cursor in 'st'
 // Override with the environment variable LOCAL_DWM_CURSOR_COLOR
-#define DEFAULT_CURSOR_COLOR "#CCCCCC"
+#define DEFAULT_CURSOR_COLOR DEFAULT_COLOR_15
 
 // The reverse color of the cursor in 'st'
 // The cursor will have this color when in a selection highlight
 // Override with the environment variable LOCAL_DWM_REV_CURSOR_COLOR
-#define DEFAULT_REV_CURSOR_COLOR "#555555"
-
-// Terminal colors 0-15
-#define DEFAULT_COLOR_00 "black"
-#define DEFAULT_COLOR_01 "red3"
-#define DEFAULT_COLOR_02 "green3"
-#define DEFAULT_COLOR_03 "yellow3"
-#define DEFAULT_COLOR_04 "#3a5fcd"
-#define DEFAULT_COLOR_05 "magenta3"
-#define DEFAULT_COLOR_06 "cyan3"
-#define DEFAULT_COLOR_07 "gray90"
-#define DEFAULT_COLOR_08 "gray50"
-#define DEFAULT_COLOR_09 "red"
-#define DEFAULT_COLOR_10 "green"
-#define DEFAULT_COLOR_11 "yellow"
-#define DEFAULT_COLOR_12 "#4f94cd"
-#define DEFAULT_COLOR_13 "magenta"
-#define DEFAULT_COLOR_14 "cyan"
-#define DEFAULT_COLOR_15 "white"
+#define DEFAULT_REV_CURSOR_COLOR DEFAULT_COLOR_08
