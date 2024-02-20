8255b5ac6047925c216e3f3bf1389e16a5303ea6
bergoid <bergoid@users.noreply.github.com>
2024-02-20 19:47:20 +0100

    Ligatures

--- src/suckless/st/config.mk
+++ src/suckless/st/config.mk
@@ -18,10 +18,12 @@ COMMONINC = ../common
 # includes and libs
 INCS = -I$(X11INC) -I${COMMONINC} \
        `$(PKG_CONFIG) --cflags fontconfig` \
-       `$(PKG_CONFIG) --cflags freetype2`
+       `$(PKG_CONFIG) --cflags freetype2` \
+       `$(PKG_CONFIG) --cflags harfbuzz`
 LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
        `$(PKG_CONFIG) --libs fontconfig` \
-       `$(PKG_CONFIG) --libs freetype2`
+       `$(PKG_CONFIG) --libs freetype2` \
+       `$(PKG_CONFIG) --libs harfbuzz`
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
