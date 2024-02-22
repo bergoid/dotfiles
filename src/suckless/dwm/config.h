89132fa9cbd1a106e898e5418a0b0684ccaa4397
bergoid <bergoid@users.noreply.github.com>
2024-02-22 15:21:32 +0100

    dwm: Alt+'=' equalizes two windows

--- src/suckless/dwm/config.h
+++ src/suckless/dwm/config.h
@@ -80,6 +80,7 @@ static Key keys[] = {
 //    { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
     { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
     { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+    { MODKEY,                       XK_equal,  setmfact,       {.f = 1.5} }, // set mfact to 0.5 absolutely
     { MODKEY,                       XK_Return, zoom,           {0} },
     { MODKEY,                       XK_Tab,    view,           {0} },
     { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
